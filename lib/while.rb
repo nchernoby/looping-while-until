576f0476fc0cba05d70b17401e49037b28da314d
Nicole Chernoby <n.chernoby@gmail.com>
2021-02-13 11:37:32 -0800

    commit tree 5e901e8c6dd4933ee5d66d837bd8889bad59f220

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,10 @@
 def using_while
   levitation_force = 6
-	
-  #your code here
+	# < 10 (return value)
+  while levitation_force < 10
+    levitation_force += 1
+    puts "Wingardium Leviosa" #{levitation_force}."
+  end
 end
 
 
